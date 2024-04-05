55eee3e2d47f871ec4b9354b4410a52da4b36641
Xinlin Zhou <XZhou@lcs.on.ca>
2024-04-05 10:02:35 -0400

    Built the innitial already.

---
+++ ToDoListApp/ToDoItem.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoItem.swift
+//  ToDoListApp
+//
+//  Created by Fiona ZHOU on 2024-04-05.
+//
+
+import SwiftUI
+
+struct ToDoItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoItem()
+}
