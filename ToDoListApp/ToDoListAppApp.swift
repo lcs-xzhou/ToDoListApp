a317f853157e79f1eb6fda45c7c2e00274b49b3f
Xinlin Zhou <XZhou@lcs.on.ca>
2024-04-05 09:32:51 -0400

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Fiona ZHOU on 2024-04-05.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
