fb191600cf4013df8db997966d40f52b94341017
Xinlin Zhou <XZhou@lcs.on.ca>
2024-04-07 00:04:24 -0400

    Built up basic supporting sub structures to avoid repetition.

---
+++ ToDoItem(Check).swift
@@ -0,0 +1,18 @@
+//
+//  ToDoItem(Check).swift
+//  ToDoListApp
+//
+//  Created by Fiona ZHOU on 2024-04-07.
+//
+
+import SwiftUI
+
+struct ToDoItem_Check_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoItem_Check_()
+}
