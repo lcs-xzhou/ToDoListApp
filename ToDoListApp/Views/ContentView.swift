23cd02900122712be5818b7705d805b0a0bc64e3
Xinlin Zhou <XZhou@lcs.on.ca>
2024-04-08 14:16:22 -0400

    Extracted a subview to handle drawing the to-do items.

---
+++ ToDoListApp/Views/ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  ToDoListApp
+//
+//  Created by Fiona ZHOU on 2024-04-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    // MARK: Stored properties
+    
+    // The item currently being added
+    @State var newItemDescription: String = ""
+    
+    // The search text
+    @State var searchText = ""
+
+    // MARK: Computed properties
+    var body: some View {
+        NavigationView {
+            VStack {
+                List {
+                    ItemView(title: "Study for Chemistry quiz", done: false)
+                    
+                    ItemView(title: "Finish Computer Science assignemnt", done: true)
+                    
+                    ItemView(title: "Go for a run aorund campus", done: false)
+                }
+                .searchable(text: $searchText)
+                
+                HStack {
+                    TextField("Enter a to-do item", text: $newItemDescription)
+                    
+                    Button("ADD") {
+                        // Add the new to-do item
+                    }
+                    .font(.caption)
+                }
+                .padding(20)
+            }
+            .navigationTitle("To do")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct ItemView: View {
+    
+    let title: String
+    let done: Bool
+    
+    var body: some View {
+        Label(
+            title: {
+                Text(title)
+            },
+            icon: { 
+                if done == true {
+                    Image(systemName: "checkmark.circle")
+                } else {
+                    Image(systemName: "circle")
+                 }
+            }
+        )
+    }
+}
