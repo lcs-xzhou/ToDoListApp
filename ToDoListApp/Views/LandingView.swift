2b9cba860eb0455a5b4dbf20e02c17533dd946f5
Xinlin Zhou <XZhou@lcs.on.ca>
2024-04-08 19:10:24 -0400

    Made it possible to add a new to-do item.

--- ToDoListApp/Views/LandingView.swift
+++ ToDoListApp/Views/LandingView.swift
@@ -34,13 +34,25 @@ struct LandingView: View {
                     
                     Button("ADD") {
                         // Add the new to-do item
+                        createToDo(withTitle: newItemDescription)
                     }
                     .font(.caption)
+                    .disabled(newItemDescription.isEmpty == true)
                 }
                 .padding(20)
             }
             .navigationTitle("To do")
         }
+    }
+    
+    // MARK: Functions
+    func createToDo(withTitle title: String) {
+        
+        // Create the new to-do item instance
+        let todo = TodoItem(title: title, done: false)
+        
+        // Append to the array
+        todos.append(todo)
     }
 }
 
